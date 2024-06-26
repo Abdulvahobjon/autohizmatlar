ba8747aeed0b467c0aeac5c8a17cad816678cad3
lxzanaa <zari.weone2010@gmail.com>
2024-06-26 14:10:10 +0300

    add

--- public/js/app.js
+++ public/js/app.js
@@ -1,7 +1,7 @@
 let header__search__box = document.querySelector("#header__search__box")
 let header__search__btn = document.querySelector("#header__search__btn")
 let header__inp = document.querySelector("#header__inp")
-header__search__btn.addEventListener("click" , function(){
+header__search__btn.addEventListener("click", function() {
     header__search__box.classList.toggle("max-w-[36px]")
     header__search__box.classList.toggle("md:max-w-[320px]")
     header__search__box.classList.toggle("max-w-[320px]")
@@ -12,23 +12,29 @@ header__search__btn.addEventListener("click" , function(){
 let res__nav___close__btn = document.querySelector("#res__nav___close__btn")
 let res__nav = document.querySelector("#res__nav")
 let hamburger__btn = document.querySelector("#hamburger__btn")
+let overlay = document.querySelector("#overlay")
+let body = document.body
 
-res__nav___close__btn.addEventListener("click" , function(){
+res__nav___close__btn.addEventListener("click", function() {
     res__nav.classList.add("right-[-100%]")
     res__nav.classList.remove("right-0")
+    overlay.classList.add("hidden")
+    body.classList.remove("h-screen")
+    body.classList.remove("overflow-hidden")
 })
-hamburger__btn.addEventListener("click" , function(){
+
+hamburger__btn.addEventListener("click", function() {
     res__nav.classList.remove("right-[-100%]")
     res__nav.classList.add("right-0")
+    overlay.classList.remove("hidden")
+    body.classList.add("h-screen")
+    body.classList.add("overflow-hidden")
 })
 
-let problem_b_card = document.querySelectorAll("#problem_b_card")
-problem_b_card.forEach(function(item, index){
-    item.addEventListener("click" , function(){
-        problem_b_card.forEach(function(item , index){
-            item.classList.remove("bg-[#E7E2E2]")
-        })
-        problem_b_card[index].classList.add("bg-[#E7E2E2]")
-    })
+overlay.addEventListener("click", function() {
+    res__nav.classList.add("right-[-100%]")
+    res__nav.classList.remove("right-0")
+    overlay.classList.add("hidden")
+    body.classList.remove("h-screen")
+    body.classList.remove("overflow-hidden")
 })
-
