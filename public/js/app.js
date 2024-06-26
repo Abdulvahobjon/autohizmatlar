46fbb13fd92c0e331f56b95378c3cae0d3865b5e
EncoderBexruzFratter <sultonovaodina06@gmail.com>
2024-06-26 14:11:37 +0500

    add

--- public/js/app.js
+++ public/js/app.js
@@ -12,8 +12,12 @@ header__search__btn.addEventListener("click" , function(){
 let res__nav___close__btn = document.querySelector("#res__nav___close__btn")
 let res__nav = document.querySelector("#res__nav")
 let hamburger__btn = document.querySelector("#hamburger__btn")
-
+let res__nav__backdrop = document.querySelector("#res__nav__backdrop")
 res__nav___close__btn.addEventListener("click" , function(){
+    res__nav.classList.add("right-[-100%]")
+    res__nav.classList.remove("right-0")
+})
+res__nav__backdrop.addEventListener("click" , function(){
     res__nav.classList.add("right-[-100%]")
     res__nav.classList.remove("right-0")
 })
@@ -30,5 +34,31 @@ problem_b_card.forEach(function(item, index){
         })
         problem_b_card[index].classList.add("bg-[#E7E2E2]")
     })
+})
+let header__big__menu__open = document.querySelector("#header__big__menu__open")
+let header__big__menu = document.querySelector("#header__big__menu")
+let header__big__menu__backdrop= document.querySelector("#header__big__menu__backdrop")
+let header = document.querySelector("#header")
+let hero = document.querySelector(".hero")
+
+header__big__menu__open.addEventListener("click" , function(){
+    header__big__menu.classList.add("top-20")
+    header__big__menu.classList.remove("bottom-[-100%]")
+    // header.classList.add("fixed")
+    // header.classList.add("top-0")
+    // header.classList.add("left-0")  
+    // header.classList.add("height-20")  
+    // hero.classList.add()
+})
+header__big__menu__backdrop.addEventListener("click" , function(){
+    header__big__menu.classList.remove("top-20")
+    header__big__menu.classList.add("bottom-[-100%]")
+})
+
+let res__big__menu = document.querySelector("#res__big__menu")
+
+res__big__menu.addEventListener("click" , function(){
+    res__big__menu.classList.toggle("h-[270px]")
+    res__big__menu.classList.toggle("h-[20px]")
 })
 
