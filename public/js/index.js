a44bf5b08060ce8559f6ccd5883681ff86ac8911
IslomjonKamolov <ikamolov2008@gmail.com>
2024-06-27 15:27:55 +0500

    buttons added for swiper, z-indexes fixed, kodes optimized

---
+++ public/js/index.js
@@ -0,0 +1,19 @@
+let btns = document.querySelectorAll("#btns");
+
+// btns.forEach(function (item, index) {
+//   item.addEventListener("click", function () {
+//     btns.forEach(function (item, index) {
+//       item.classList.remove("active_button");
+//     });
+//     item.classList.add("active_button");
+//   });
+// });
+
+document.querySelectorAll("#btns").forEach((item) => {
+  item.addEventListener("click", () => {
+    document
+      .querySelectorAll("#btns")
+      .forEach((btn) => btn.classList.remove("active_button"));
+    item.classList.add("active_button");
+  });
+});
