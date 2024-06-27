994ef258e3fbc0f6298821c93c455a9a9c808090
DiyorbekTursunov <diyorbektursunov570@gmail.com>
2024-06-27 17:41:10 +0500

    fix bug

--- public/js/catalog.js
+++ public/js/catalog.js
@@ -1,16 +1,19 @@
 let filter__catalog = document.querySelector("#filter__catalog")
 let catalog__res = document.querySelector("#catalog__res")
 let catalog__right = document.querySelector("#catalog__right")
+let catslog__left = document.querySelector("#catslog__left")
 
-console.log(catalog__right);
 
 filter__catalog.addEventListener("click" , function(){
     filter__catalog.classList.toggle("mb-12")
     catalog__res.classList.toggle("hidden")
     catalog__res.classList.toggle("flex")
+
     catalog__right.classList.toggle("hidden")
     catalog__right.classList.toggle("catalog__right_active")
     catalog__right.classList.toggle("grid")
+
+    catslog__left.classList.toggle("hidden")
 })
 let catalog__left__box = document.querySelectorAll("#catalog__left__box")
 
